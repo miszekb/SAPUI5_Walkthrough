c6406641ef8b6a70138026560f5ca9f0b18e9dd2
miszekb <miszekb@gmail.com>
2023-07-24 07:32:46 +0200

    initial commit

---
+++ webapp/controller/HelloPanel.controller.js
@@ -0,0 +1,20 @@
+sap.ui.define([
+    "sap/ui/core/mvc/Controller",
+    "sap/m/MessageToast",
+    "sap/ui/core/Fragment"
+], function(Controller, MessageToast, Fragment) {
+    'use strict';
+    
+    return Controller.extend("sap.ui.demo.walkthrough.controller.HelloPanel", {
+        onShowHello: function() {
+            // read msg from i18n model
+            let oBundle = this.getView().getModel("i18n").getResourceBundle();
+            let sRecipient = this.getView().getModel().getProperty("/recipient/name");
+            const sMsg = oBundle.getText("helloMsg", [sRecipient]);
+            MessageToast.show(sMsg);
+        },
+        onOpenDialog: function() {
+            this.getOwnerComponent().openHelloDialog();
+        }
+    });
+})
